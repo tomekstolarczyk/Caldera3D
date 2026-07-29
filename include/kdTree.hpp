e5a68ed414e15de34c4f3c2d1ea3a1ac227d3d82
tomekstolarczyk <tomasz.d.stolarczyk@gmail.com>
2026-07-29 00:32:19 -0600

    kd tree node structure

---
+++ include/kdTree.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include "PointCloud.hpp"
+#include <memory>
+
+struct KdTreeNode
+{
+    Point3D point;
+    int cloudIndex; // index punktu w oryginalnej chmurze
+    int axis;       // wspolrzedna po ktorej tniemy przestrzen x / y / z
+    std::unique_ptr<KdTreeNode> left;
+    std::unique_ptr<KdTreeNode> right;
+
+    // member initializer list
+    KdTreeNode(Point3D pt, int idx, int ax)
+        : point(pt), cloudIndex(idx), axis(ax), left(nullptr), right(nullptr)
+    {
+    }
+};
+
+class KdTree
+{
+    std::unique_ptr<KdTreeNode> root;
+
+    // Median-based construction - zamiast klasycznego insertowania po kolei - mamy posortowane dane
+    // gwarantuje idealnie zbalansowane drzewo
+    // - left / right - rekurencyjny zakres budowy
+    // - depth - os ciecia - x / y / z
+    std::unique_ptr<KdTreeNode> buildRecursive(std::vector<std::pair<Point3D, int>> &cloud,
+                                               int left, int right, int depth);
+
+  public:
+    // member initializer list
+    KdTree() : root(nullptr) {}
+
+    void build(const std::vector<Point3D> &cloud);
+};
