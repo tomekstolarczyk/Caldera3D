1417ee1d764181f7b52e18edb3edf143d813f0a3
tomekstolarczyk <tomasz.d.stolarczyk@gmail.com>
2026-08-27 02:44:32 -0600

    ransac should be working now

--- include/Math.hpp
+++ include/Math.hpp
@@ -14,12 +14,12 @@ inline float distSquared(const Point3D &a, const Point3D &b)
     return dx * dx + dy * dy + dz * dz;
 }
 
-inline Point3D subVectors(const Point3D &a, const Point3D &b)
+inline Point3D pointVecSubstraction(const Point3D &a, const Point3D &b)
 {
     return {a.x - b.x, a.y - b.y, a.z - b.z};
 }
 
-inline Point3D crossProduct(const Point3D &v1, const Point3D &v2)
+inline Point3D pointVecCrossProduct(const Point3D &v1, const Point3D &v2)
 {
     return {(v1.y * v2.z) - (v1.z * v2.y), (v1.z * v2.x) - (v1.x * v2.z),
             (v1.x * v2.y) - (v1.y * v2.x)};
--- src/Ransac.cpp
+++ src/Ransac.cpp
@@ -6,19 +6,26 @@ std::vector<Point3D> findPlanes(const std::vector<Point3D> &cloud, int k, float 
 {
     // okej a wiec  PRZEPIS:
     // x 1 losujemy 3 punkty z chmury
-    // - 2 tworzymy rownanie plaszczyzny
-    // - 3 dla kazdego punktu sprawdzamy czy lezy na plaszyznie niej wedlug threshold
-    // - 4 liczymy ile jest inlinerow
-    // x 5 powtarzamy proces k razy
-    // - 6 wybieramy najlepsza
-    // - 7 update : adaptacyjna liczba iteracji
+    // x 2 tworzymy rownanie plaszczyzny
+    // x 3 dla kazdego punktu sprawdzamy czy lezy na plaszyznie wedlug threshold i zliczamy
+    // inlinerow
+    // x 4 powtarzamy proces k razy
+    // x 5 wybieramy najlepsza iteracje
+    // - 7 update : adaptacyjna liczba iteracji : TODO
 
     // =============================================================================
+
+    if (cloud.size() < 3)
+    {
+        return {};
+    }
 
     // rng
     std::mt19937 mt{std::random_device{}()};
     std::uniform_int_distribution<int> dist(0, cloud.size() - 1);
 
+    // 5 wybieramy najlepsza
+    std::vector<Point3D> inlinersBest;
     int maxCount = 0;
 
     // 4 powtarzamy proces k razy
@@ -44,18 +51,47 @@ std::vector<Point3D> findPlanes(const std::vector<Point3D> &cloud, int k, float 
         planePoints.push_back(cloud[idx2]);
         planePoints.push_back(cloud[idx3]);
 
-        // 2 tworzymy rownanie plaszczyzny
-        Point3D vec1 = Math3D::subVectors(planePoints[1], planePoints[0]);
-        Point3D vec2 = Math3D::subVectors(planePoints[2], planePoints[0]);
-        Point3D cross = Math3D::crossProduct(vec1, vec2);
-        // tutaj znajdziemy A,B,C i D zaraz i mamy plaszczyzne
+        // 2 tworzymy rownanie plaszczyzny Ax + By + Cz + D = 0
+        Point3D vec1 = Math3D::pointVecSubstraction(planePoints[1], planePoints[0]);
+        Point3D vec2 = Math3D::pointVecSubstraction(planePoints[2], planePoints[0]);
+        Point3D cross = Math3D::pointVecCrossProduct(vec1, vec2);
+        float A = cross.x;
+        float B = cross.y;
+        float C = cross.z;
+        float D = -(A * planePoints[0].x + B * planePoints[0].y + C * planePoints[0].z);
 
-        // 3 threshold - odleglosc od plasczyzny
-        // 4 zliczamy inlinerow
-        int count = 0;
+        // 3 dla kazdego punktu sprawdzamy czy lezy na plaszyznie wedlug threshold i zliczamy
+        // inlinerow
+        float denom = std::sqrt(A * A + B * B + C * C);
+
+        // jesli wylosowalismy 3 punkty w jednej linii to ich iloczyn wektorowy bedzie zerowy
+        // takie punkty i tak sa nam do niczego bo nie zbudujemy na nich plaszczyzny wiec wywalamy
+        // ta iteracje do smieci straight up
+        if (denom < 1e-6f)
+        {
+            continue;
+        }
+
+        std::vector<Point3D> inliners;
+        inliners.reserve(cloud.size());
         for (int i = 0; i < cloud.size(); i++)
         {
-            // TODO
+
+            float nom = std::abs(A * cloud[i].x + B * cloud[i].y + C * cloud[i].z + D);
+            float d = nom / denom;
+
+            if (d <= threshold)
+            {
+                inliners.push_back(cloud[i]);
+            }
+        }
+
+        if (inliners.size() > maxCount)
+        {
+            maxCount = static_cast<int>(inliners.size());
+            inlinersBest = std::move(inliners);
         }
     }
+
+    return inlinersBest;
 }
