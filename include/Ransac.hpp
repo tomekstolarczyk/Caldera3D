185e340998356a2a47940acb3ad3963b15590d9e
tomekstolarczyk <tomasz.d.stolarczyk@gmail.com>
2026-08-30 01:25:49 -0600

    ransac updates

--- include/Ransac.hpp
+++ include/Ransac.hpp
@@ -3,6 +3,16 @@
 #include <utility> // std::pair
 #include <vector>
 
-// para wektorow: {Inliers, Outliers}
-std::pair<std::vector<Point3D>, std::vector<Point3D>> findPlanes(const std::vector<Point3D> &cloud,
-                                                                 int k, float threshold);
+struct ransacResult
+{
+    std::vector<Point3D> inliers;
+    std::vector<Point3D> outliers;
+    float A, B, C, D;
+};
+
+// znajduje jedna plaszyzne
+ransacResult findPlanes(const std::vector<Point3D> &cloud, int k, float threshold);
+
+// znajduje wszystkie wielkie plaszyzny - podloga, stol, sciany
+std::vector<ransacResult> ransacBulk(const std::vector<Point3D> &cloud, int k, float threshold,
+                                     int minPlaneSize);
