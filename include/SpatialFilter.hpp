fe40991c7cf7d47ae2d530308aa17a641ab9b8f3
tomekstolarczyk <tomasz.d.stolarczyk@gmail.com>
2026-08-30 21:17:28 -0600

    next things coming

---
+++ include/SpatialFilter.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "PointCloud.hpp"
+
+std::vector<Point3D> everythingOnTheTable(const std::vector<std::vector<Point3D>> &dataClusters,
+                                          float ATable, float BTable, float CTable, float DTable);
